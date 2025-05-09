acfe26d97e276f60e46848be3aa66efdf06a088c
zhenghuahou <2430370966@qq.com>
2025-05-09 14:41:31 +0800

    feat: 提交本地代码

---
+++ bin/test.mjs
@@ -0,0 +1,9 @@
+import { Buffer } from 'node:buffer';
+
+const aa = Buffer.from((true ? '05' : ''))
+
+
+const buf4 = Buffer.from([1, 2, 3]);
+
+console.info('aa:',aa,' --:',aa.toString())
+// console.info(' buf4:',buf4)
